996440c670a17c3955850f87ea480f82cc325e12
yxnzhe <yanzhe2003@gmail.com>
2022-08-30 17:59:42 +0800

    vue and django

---
+++ midags_frontend/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="wrapper">
+    <nav class="navbar is-light">
+      <div class="navbar-brand"></div>
+      <router-link to="/" class="navbar-item"><strong>MiDAgS™</strong></router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+  @import '../node_modules/bulma';
+</style>
